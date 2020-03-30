98baddcb6c13faf79f4cecced82f900a730bdd53
Maxim Sednev <darkfreider@gmail.com>
2020-03-30 23:18:19 +0300

    added pic reinitialization and remap

--- HELLO.C
+++ HELLO.C
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include <dos.h>
 
 
@@ -21,8 +22,12 @@ void pic_send_eoi(unsigned char irq)
 #define OLD_MASTER_OFFSET 0x8
 #define OLD_SLAVE_OFFSET 0x70
 
-#define NEW_MASTER_OFFSET 0
-#define NEW_SLAVE_OFFSET 0
+// TODO(max): redefine them according to your assignment variant
+// 0x8 -> A8h for master
+// 0x70 -> ? for slave, I think i need to pick random
+
+#define NEW_MASTER_OFFSET 0xa8
+#define NEW_SLAVE_OFFSET OLD_SLAVE_OFFSET
 
 #define PIT_IRQ 0
 #define KBD_IRQ 1
@@ -126,7 +131,28 @@ void interrupt new_mouse_int(void)
 	old_int_handlers[MOUSE_IRQ].int_ptr();
 }
 
-#define GENERATE_GENERIC_INT_HANDLER_NAME(N) new_generic_ ## N ## int
+void interrupt new_pit_int(void)
+{
+	VIDEO far *screen = (VIDEO far *)MK_FP(0xb800, 0);
+	
+	char *msg = "PIT PIT PIT";
+	char *c = msg;
+	int n = 0;
+	
+	while (*c != 0)
+	{
+		screen[n].symbol = *c++;
+		screen[n].attribute = color_table[0];
+		n++;
+	}
+	
+	old_int_handlers[PIT_IRQ].int_ptr();
+}
+
+#define t(N) \
+	void interrupt new_generic_ ## N ## _int(void) { print_status(); old_int_handlers[N].int_ptr(); }
+
+#define GENERATE_GENERIC_INT_HANDLER_NAME(N) new_generic_ ## N ## _int
 #define GENERATE_GENERIC_INT_HANDLER(N) void interrupt new_generic_ ## N ## _int(void)
 
 GENERATE_GENERIC_INT_HANDLER(0) { old_int_handlers[0].int_ptr(); }
@@ -148,31 +174,69 @@ GENERATE_GENERIC_INT_HANDLER(14)  { old_int_handlers[14].int_ptr(); }
 GENERATE_GENERIC_INT_HANDLER(15)  { old_int_handlers[15].int_ptr(); }
 
 Int_description new_int_handlers[] = {
-	{ NEW_MASTER_OFFSET + 0, GENERATE_GENERIC_INT_HANDLER(0) },
+	{ NEW_MASTER_OFFSET + 0, GENERATE_GENERIC_INT_HANDLER_NAME(0) },
 	{ NEW_MASTER_OFFSET + 1, new_kbd_int },
-	{ NEW_MASTER_OFFSET + 2, GENERATE_GENERIC_INT_HANDLER(2) },
-	{ NEW_MASTER_OFFSET + 3, GENERATE_GENERIC_INT_HANDLER(3) },
-	{ NEW_MASTER_OFFSET + 4, GENERATE_GENERIC_INT_HANDLER(4) },
-	{ NEW_MASTER_OFFSET + 5, GENERATE_GENERIC_INT_HANDLER(5) },
-	{ NEW_MASTER_OFFSET + 6, GENERATE_GENERIC_INT_HANDLER(6) },
-	{ NEW_MASTER_OFFSET + 7, GENERATE_GENERIC_INT_HANDLER(7) },
+	{ NEW_MASTER_OFFSET + 2, GENERATE_GENERIC_INT_HANDLER_NAME(2) },
+	{ NEW_MASTER_OFFSET + 3, GENERATE_GENERIC_INT_HANDLER_NAME(3) },
+	{ NEW_MASTER_OFFSET + 4, GENERATE_GENERIC_INT_HANDLER_NAME(4) },
+	{ NEW_MASTER_OFFSET + 5, GENERATE_GENERIC_INT_HANDLER_NAME(5) },
+	{ NEW_MASTER_OFFSET + 6, GENERATE_GENERIC_INT_HANDLER_NAME(6) },
+	{ NEW_MASTER_OFFSET + 7, GENERATE_GENERIC_INT_HANDLER_NAME(7) },
 	
-	{ NEW_SLAVE_OFFSET + 0, GENERATE_GENERIC_INT_HANDLER(8) },
-	{ NEW_SLAVE_OFFSET + 1, GENERATE_GENERIC_INT_HANDLER(9) },
-	{ NEW_SLAVE_OFFSET + 2, GENERATE_GENERIC_INT_HANDLER(10) },
-	{ NEW_SLAVE_OFFSET + 3, GENERATE_GENERIC_INT_HANDLER(11) },
+	{ NEW_SLAVE_OFFSET + 0, GENERATE_GENERIC_INT_HANDLER_NAME(8) },
+	{ NEW_SLAVE_OFFSET + 1, GENERATE_GENERIC_INT_HANDLER_NAME(9) },
+	{ NEW_SLAVE_OFFSET + 2, GENERATE_GENERIC_INT_HANDLER_NAME(10) },
+	{ NEW_SLAVE_OFFSET + 3, GENERATE_GENERIC_INT_HANDLER_NAME(11) },
 	{ NEW_SLAVE_OFFSET + 4, new_mouse_int },
-	{ NEW_SLAVE_OFFSET + 5, GENERATE_GENERIC_INT_HANDLER(13) },
-	{ NEW_SLAVE_OFFSET + 6, GENERATE_GENERIC_INT_HANDLER(14) },
-	{ NEW_SLAVE_OFFSET + 7, GENERATE_GENERIC_INT_HANDLER(15) },
-}
+	{ NEW_SLAVE_OFFSET + 5, GENERATE_GENERIC_INT_HANDLER_NAME(13) },
+	{ NEW_SLAVE_OFFSET + 6, GENERATE_GENERIC_INT_HANDLER_NAME(14) },
+	{ NEW_SLAVE_OFFSET + 7, GENERATE_GENERIC_INT_HANDLER_NAME(15) },
+};
 
+
+#define ICW1_INIT 0x10
+#define ICW1_ICW4 0x01
+#define ICW1_INTERVAL4 0x4
+#define ICW4_8086 0x1
+
+
+void pic_remap(int master_offset, int slave_offset)
+{
+	unsigned char master_mask;
+	unsigned char slave_mask;
+
+	assert(master_offset % 8 == 0);
+	assert(slave_offset % 8 == 0);
+	
+	master_mask = inp(PIC1_DATA);
+	slave_mask = inp(PIC2_DATA);
+	
+	// NOTE(max): ICW1
+	outb(PIC1_COMMAND, ICW1_INIT | ICW1_INTERVAL4 | ICW1_ICW4);
+	outb(PIC2_COMMAND, ICW1_INIT | ICW1_INTERVAL4 | ICW1_ICW4);
+	
+	// NOTE(max): ICW2
+	outb(PIC1_DATA, master_offset);
+	outb(PIC2_DATA, slave_offset);
+	
+	// NOTE(max): ICW3
+	outb(PIC1_DATA, 0x4);
+	outb(PIC2_DATA, 0x2);
+	
+	// NOTE(max): ICW4
+	outb(PIC1_DATA, ICW4_8086);
+	outb(PIC2_DATA, ICW4_8086);
+	
+	// NOTE(max): restore interrupt mask
+	outp(PIC1_DATA, master_mask);
+	outp(PIC2_DATA, slave_mask);
+}
 
 void init(void)
 {
 	int i = 0;
 	
-	// TODO(max): collapse into single loop
+	// NOTE(max): save old interrupt handlers into a table
 	for (i = 0; i < 8; i++)
 	{
 		old_int_handlers[i].int_num = OLD_MASTER_OFFSET + i;
@@ -184,8 +248,18 @@ void init(void)
 		old_int_handlers[i + 8].int_ptr = getvect(OLD_SLAVE_OFFSET + i);
 	}
 	
-	setvect(OLD_MASTER_OFFSET + KBD_IRQ, new_kbd_int);
-	setvect(OLD_SLAVE_OFFSET + MOUSE_IRQ - 8, new_mouse_int);
+	for (i = 0; i < 8; i++)
+	{
+		setvect(NEW_MASTER_OFFSET + i, new_int_handlers[i].int_ptr);
+	}
+	for (i = 0; i < 8; i++)
+	{
+		setvect(NEW_SLAVE_OFFSET + i, new_int_handlers[i + 8].int_ptr);
+	}
+	//setvect(OLD_MASTER_OFFSET + KBD_IRQ, new_kbd_int);
+	//setvect(OLD_SLAVE_OFFSET + MOUSE_IRQ - 8, new_mouse_int);
+	
+	pic_remap(NEW_MASTER_OFFSET, OLD_SLAVE_OFFSET);
 }
 
 int main(void)
